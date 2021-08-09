e2642ebe086963ea876ef52577d39dc789c0605c
Jardel_Silva <jardeloliver197@gmail.com>
2021-08-09 13:55:11 -0300

    Atualizando enunciado dos  desafios da aula 01

--- bimestre02/front-end02/aula01/mesa01-etapa2.js
+++ bimestre02/front-end02/aula01/mesa01-etapa2.js
@@ -1,4 +1,17 @@
-/* Terminada esta etapa é hora de preparar o código para ficar mais amigável ao usuário final,
+/*A secretaria de uma escola contratou a sua empresa para desenvolver sua plataforma de
+gestão de alunos, sendo uma dessas partes a criação de um sistema que calcule as notas
+obtidas ao longo do semestre, Você e seu grupo fazem parte da equipe responsável por
+implementar esta funcionalidade no sistema.
+Para isso, crie um script que permita adicionar uma sequência de números (um array de
+números) consecutivamente para receber as notas dos 4 bimestres. Após isso, crie uma
+estrutura na qual todos os números do array sejam somados sequencialmente: o primeiro
+número seja somado ao segundo e o resultado seja impresso no console. Então,
+temos que pegar esse resultado e adicionar o terceiro número a ele, e assim por
+diante, até terminarmos de percorrer o array. Seguindo a lógica do exemplo a seguir:
+
+Por fim, a soma desses números deve ser dividida pelo total de bimestres (4) para calcular a
+média e guardada em uma variável, a qual deve ser exibida no console.
+Terminada esta etapa é hora de preparar o código para ficar mais amigável ao usuário final,
 com mensagens que informem a nota do aluno e sua condição:
 1. Utilizando a função console.log informe a nota que o aluno teve em cada um dos
 bimestres e por fim sua nota final. Por exemplo: “Parabéns pela conclusão do primeiro
