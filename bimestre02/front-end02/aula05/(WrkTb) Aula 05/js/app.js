e75d976c30d79bda4e93d3995d0afe1a3400cadd
Jardel_Silva <jardeloliver197@gmail.com>
2021-08-18 17:13:40 -0300

    Melhorias em botão DarkMode

--- bimestre02/front-end02/aula05/(WrkTb) Aula 05/js/app.js
+++ bimestre02/front-end02/aula05/(WrkTb) Aula 05/js/app.js
@@ -1,6 +1,7 @@
 let btn = document.querySelector(".tema");
 let contador = 1;
 let circulo = document.querySelector(".circulo");
+let circuloWhite = document.querySelector("#noturno");
 btn.addEventListener("click", (event) => {
     event.preventDefault();
     contador++;
@@ -9,13 +10,15 @@ btn.addEventListener("click", (event) => {
         btn.classList.remove(".tema");
         corSite = document.body.style.background = "#201e1e";
         title = document.body.style.color = "white";
-        circulo.style.animation = "deslizar-1 0.5s ease forwards";
+        circulo.style.animation = "deslizar-1 0.3s ease forwards";
         btn.style.background = "white";
+        circuloWhite.classList.add("circulo-2");
     } else{
-        corSite = document.body.style.background = "white";
+        corSite = document.body.style.background = "rgb(219, 219, 219";
         title = document.body.style.color = "#201e1e";
-        circulo.style.animation = "deslizar-2 0.5s ease forwards";
+        circulo.style.animation = "deslizar-2 0.3s ease forwards";
         btn.style.background = "rgb(44, 6, 80)";
+        circuloWhite.classList.remove("circulo-2");
     }
 
 })
