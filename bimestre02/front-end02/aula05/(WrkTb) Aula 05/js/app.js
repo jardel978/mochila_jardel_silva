4821dae977dce729e5a73a2ece6d60e003c56d46
Jardel_Silva <jardeloliver197@gmail.com>
2021-08-18 16:29:53 -0300

    Adicionando resolução de exercício Aula05

---
+++ bimestre02/front-end02/aula05/(WrkTb) Aula 05/js/app.js
@@ -0,0 +1,21 @@
+let btn = document.querySelector(".tema");
+let contador = 1;
+let circulo = document.querySelector(".circulo");
+btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    contador++;
+    
+    if(contador % 2 == 0){
+        btn.classList.remove(".tema");
+        corSite = document.body.style.background = "#201e1e";
+        title = document.body.style.color = "white";
+        circulo.style.animation = "deslizar-1 0.5s ease forwards";
+        btn.style.background = "white";
+    } else{
+        corSite = document.body.style.background = "white";
+        title = document.body.style.color = "#201e1e";
+        circulo.style.animation = "deslizar-2 0.5s ease forwards";
+        btn.style.background = "rgb(44, 6, 80)";
+    }
+
+})
