e1f42690834d336c1008fb8b2e53de1a1478a230
Jardel_Silva <jardeloliver197@gmail.com>
2021-10-25 12:45:21 -0300

    Adicionando resolução de exercício

---
+++ bimestre03/frontend3/aula04/app-limpando-projeto/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src="./images/homem-feliz-dinheiro.jpg"></img>
+    </div>
+  );
+}
+
+export default App;
