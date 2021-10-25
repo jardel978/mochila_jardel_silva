6f0f4f53e8febad8d7101fa1bd85f8715989434e
Jardel_Silva <jardeloliver197@gmail.com>
2021-10-25 17:51:05 -0300

    Corrigindo pasta

---
+++ bimestre03/backend1/aula07-FlyweightPattern/mesa/src/com/digitalhouse/MesaEstudos/ArvoreException.java
@@ -0,0 +1,13 @@
+package com.digitalhouse.MesaEstudos;
+
+public class ArvoreException extends Exception{
+
+    public ArvoreException (String mensageFroUser) {
+        super(mensageFroUser);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
